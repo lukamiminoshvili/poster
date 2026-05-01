2875e7e5a8ed89124db61bff4c07c7f3ba50e0d5
lukemiminoshvili <lukemiminoshvili@itvet.ge>
2026-05-01 19:56:53 +0400

    we have added http sever of our program

--- poster.js
+++ poster.js
@@ -68,3 +68,13 @@ cron.schedule('0 14 * * *', () => {
 
 // პირველი გაშვება ტესტისთვის (შეგიძლია წაშალო, როცა დარწმუნდები რომ მუშაობს)
 postToFacebook();
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+     res.writeHead(200, { 'Content-Type': 'text/plain' });
+     res.end('Bot is running!');
+}).listen(port, () => {
+     console.log(`Server is listening on port ${port}`);
+});
